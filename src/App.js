37b9b9909dbae4dc9f474ebd085d25bf381acbd5
AndreyKondakov <primax7k@gmail.com>
2019-02-28 17:46:44 +0200

    add settings for deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
-
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Resume from './components/Resume/Resume';
